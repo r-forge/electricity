90e8c3b156da2ec709bd672de36fe58818cf4c85
rbrt (Robert Ferstl) <datarob@gmx.at>
2007-12-17 10:46:48 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/electricity@100 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/demo/demandcurve.R
@@ -0,0 +1,51 @@
+
+load("load.Rdata")
+load("eex.Rdata")
+
+# total electricty demand
+totload <- load$Austria + load$Germany
+
+
+#par(mfrow=c(2,1))
+
+# plot total load values
+plot(c(t(totload)),type="l", col = "blue",
+	main = "Hourly load values for 2006 (in MWh)",
+	xlab = "Hours",
+	ylab = "Load in MW")
+	
+
+pdf(file="loaddurationcurve.pdf")
+# plot total load values
+plot(sort(c(t(totload)),decreasing=T,na.last=T), type="l",lwd=3,col = "green",
+	main = "Load duration curve for 2006 (in MWh)",
+	xlab = "Hours per year",
+	ylab = "Load in MW")
+
+dev.off()
+
+# plot EEX trading volumes
+plot(c(t(eex$volumes)),type="l",col = "red",
+	main = "Spot market trading volumes at EEX for 2006 (in MWh)",
+	xlab = "Hours",
+	ylab = "Load in MW")
+
+# plot EEX prices
+plot(c(t(eex$prices)),type="l",col = "red",
+	main = "Spot market prices at EEX for 2006 (per MWh)",
+	xlab = "Hours",
+	ylab = "Price per MWh")
+
+
+plot(c(t(totload)),c(t(eex$prices)),col = "blue",
+main = "Electricity demand curve for 2006",
+xlab = "Hourly load values (in MWh)",
+ylab = "Spot market prices at EEX(per MWh)")
+
+
+plot(c(t(eex$volumes)),c(t(eex$prices)),col = "blue",
+main = "Electricity demand curve for 2006",
+xlab = "EEX spot market trading volumes (in MWh)",
+ylab = "Spot market prices at EEX(per MWh)")
+
+dev.off()
