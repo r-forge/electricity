b51b0c6fa2d8d29239a2996af7691051c08295ce
rbrt (Robert Ferstl) <datarob@gmx.at>
2008-04-21 01:03:33 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/electricity@259 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- models/dynamicmcp/monopoly.R
+++ models/dynamicmcp/monopoly.R
@@ -23,6 +23,21 @@ points(2*q_oli,demand(2*q_oli),col="blue")
 q_pc <- (alpha-c)/beta
 points(q_pc,demand(q_pc),col="green")
 
+# price cap
+
+pc = 30
+
+q = 1:100
+
+plot(demand(q)*q-c*q,type="l")
+
+pcs = c(47,48,49,50)
+for (i in 1:length(pcs)) {
+	pc = pcs[i]
+lines(q,apply(cbind(demand(q),rep(pc,length(1))),1,min)*q-c*q,lty=2,lwd=2)
+}
+
+points(q_opt,demand(q_opt)*q_opt-c*q_opt,col="red",lwd="2")
 
 
 
