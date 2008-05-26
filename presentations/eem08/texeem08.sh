e86ffe726ce30291fd737661f436707f1708085d
rbrt (Robert Ferstl) <datarob@gmx.at>
2008-05-26 10:50:16 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/electricity@322 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ presentations/eem08/texeem08.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+latex eem08_burger_ferstl
+latex eem08_burger_ferstl
+dvipdf eem08_burger_ferstl
+rm *.aux
+rm *.blg
+rm *.log
+rm *.bbl
+rm *.dvi
+rm *.out
+rm *.snm
+rm *.toc
+rm *.nav
