34a0250a25f27f680561261412796d47abe806a5
rbrt (Robert Ferstl) <datarob@gmx.at>
2008-05-28 11:38:36 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/electricity@329 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ documents/texnumericalpaper.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+latex numericalpaper
+bibtex numericalpaper
+latex numericalpaper
+latex numericalpaper
+dvipdf numericalpaper
+rm numericalpaper.aux
+rm numericalpaper.blg
+rm numericalpaper.log
+rm numericalpaper.bbl
+rm numericalpaper.dvi
+rm numericalpaper.out
+rm numericalpaper.rel
